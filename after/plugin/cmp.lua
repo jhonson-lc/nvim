09329008af46c558cc8fe38e0126c0bf78e792e4
jhonson-lc <jhoncisneros11@gmail.com>
2022-02-21 09:40:35 -0500

    "feat: add language server for typescript"

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -18,6 +18,8 @@ cmp.setup({
       end,
     },
     mapping = {
+      ['<C-p>'] = cmp.mapping.select_prev_item(),
+      ['<C-n>'] = cmp.mapping.select_next_item(),
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
@@ -26,24 +28,6 @@ cmp.setup({
         behavior = cmp.ConfirmBehavior.Replace,
         select = true
       }),
-      ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-        else
-        fallback()
-        end
-      end,
-      ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
